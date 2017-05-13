322f2c505b26575cffbbe7ce56dd3d06b2f9a49b
RayCJ92 <raycj912@gmail.com>
2017-05-14 01:15:04 +0800

    #198 houseRobber

--- HouseRobber.java
+++ HouseRobber.java
@@ -7,7 +7,7 @@
 
 // Subscribe to see which companies asked this question.
 
-//Dynamic Programming
+//#1 Dynamic Programming
 public class Solution {
     public int rob(int[] nums) {
         if (nums == null || nums.length == 0)
@@ -21,5 +21,40 @@ public class Solution {
             dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
         }
         return dp[nums.length-1];
+    }
+}
+
+//#2 use 2 variables
+class Solution2 {
+    public int rob(int[] nums) {
+        int robCurrent = 0;
+        int noCurrent = 0;
+        for (int i = 0; i < nums.length; i++){
+            int temp = robCurrent;                                  
+            robCurrent = Math.max(nums[i]+noCurrent,robCurrent);  //max of robbing ith house
+            noCurrent = temp;                                       //max of not robbing ith house(in face,it is the money rob last time)
+        }
+        return Math.max(noCurrent,robCurrent);
+    }
+}
+
+//#3 use recursion (bad code, OJ timeout)
+class Solution3 {
+   public int rob(int[] nums){
+        return dp(nums.length, nums);
+    }
+
+    int dp(int n, int[] nums){
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        if (n == 2)
+            return nums[0]>nums[1]?nums[0]:nums[1];
+        else {
+            int dp1 =(nums[n-1]+dp(n-2, nums));
+            int dp2 = dp(n-1,nums);
+            return dp1>dp2?dp1:dp2;
+        }
     }
 }
